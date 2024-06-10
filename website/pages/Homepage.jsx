ea027921c20b97e8360d669a7c7c971c0ad02895
Pratyush Kamal <pratyushkamal88@gmail.com>
2024-06-10 15:26:14 +0530

    update

--- website/pages/Homepage.jsx
+++ website/pages/Homepage.jsx
@@ -1,20 +1,18 @@
-// App.js
 import React from 'react';
 import Myself from '../components/Myself';
 import Whoami from '../components/Whoami';
 import Projecthome from '../components/Projecthome';
 import Sayhello from '../components/Sayhello';
-import './Homepage.module.css';
 
-function App() {
+function Homepage() {
   return (
-    <div className="container">
+    <>
       <Myself />
       <Whoami />
       <Projecthome />
       <Sayhello />
-    </div>
+    </>
   );
 }
 
-export default App;
+export default Homepage;
