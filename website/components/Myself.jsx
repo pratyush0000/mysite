bcc6f299a6eb8c9237f55e3b6621e7f65c66db2e
Pratyush Kamal <pratyushkamal88@gmail.com>
2024-06-10 14:29:34 +0530

    update

--- website/components/Myself.jsx
+++ website/components/Myself.jsx
@@ -1,12 +1,21 @@
-// Myself.jsx
 import React from 'react';
 import styles from './Myself.module.css'; // Import the CSS module
+import myPhoto from '../assets/me.png'; // Import your photo
 
 const Myself = () => {
   return (
-    <div className={styles["full-screen-div"]} style={{ backgroundColor: 'lightblue' }}>
-            <div className={styles.leftdiv}></div>
-            <div className={styles.rightdiv}></div>
+    <div className={styles["full-screen-div"]}>
+      <img src={myPhoto} className={styles.myPhoto} alt="My Photo" />
+        <div className={styles.leftdiv}>
+            <div className={styles.titlehome}>
+                Frontend<br/>
+                Dev/Des.
+            </div>
+            <div className={styles.subtexthome}>
+                I blend creativity with precision in crafting seamless user interactions.
+            </div>
+        </div>
+        <div className={styles.rightdiv}></div>
     </div>
   );
 }
